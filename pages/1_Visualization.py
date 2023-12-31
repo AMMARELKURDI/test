2658498dd2a26ef5772baa1e94dd71560b4b7efe
AMMARELKURDI <155187194+AMMARELKURDI@users.noreply.github.com>
2023-12-31 04:48:31 +0200

    Add files via upload

--- pages/1_Visualization.py
+++ pages/1_Visualization.py
@@ -15,6 +15,7 @@ if file is not None:
     num_row=st.slider("Choose Num Of Rows:",max_value=len(df),min_value=1,step=1)
     Selecting_Columns=st.multiselect("Select Columns",options =df.columns.to_list(),default=df.columns.to_list())
     st.write(df[:num_row][Selecting_Columns])
+    df=df[:num_row][Selecting_Columns]
 
     numeric_col=df.select_dtypes(include=np.number).columns.to_list()
     string_col=df.select_dtypes(include="object",exclude='datetime').columns.to_list()
@@ -28,8 +29,10 @@ if file is not None:
         with c2:
             y_col=st.selectbox("Select Y-Axis",numeric_col)
         with c3:
+            numeric_col.append(None)
             size_Bubble=st.selectbox("Select Bubble Size",numeric_col)
         with c4:
+            string_col.append(None)
             clor=st.selectbox("Select Bubble Color",string_col)
 
         fig_scatter=px.scatter(df,x=x_col,y=y_col,color=clor,size=size_Bubble)
